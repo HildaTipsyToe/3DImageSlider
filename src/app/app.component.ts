e6f5a8ae41a00093c2c2ae08050bb0506fc55dc9
Jeppe <88774896+HildaTipsyToe@users.noreply.github.com>
2023-04-20 14:21:21 +0200

    everything

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ImageScrolling3D';
+
 }
+
+var counter: number = 0;
+window.onwheel = e =>{
+  if(e.deltaY > 0){
+
+    counter += 10;
+    console.log("DownwardsDog");
+    console.log(counter);
+    
+  }
+  if(e.deltaY < 0){
+    if(counter === 0){
+      counter = 360
+    }
+    counter -= 10;
+    console.log("Upwardsdog"); 
+    console.log(counter);
+       
+  }
+  counter = Math.max(Math.min(counter, 360), 0)
+  document.getElementById('gallery_box_outer')!.style.transform = `perspective(1000px) rotateX(-8deg) rotateY(${-counter}deg)`;
+  
+}
+
